c41b0be4c38b052c2e0b3aee771a6c195400744e
Dom Williams <dwi3774@googlemail.com>
2015-11-28 17:25:35 +0000

    Added base game class

---
+++ src/memes/Game.java
@@ -0,0 +1,16 @@
+package memes;
+
+public class Game {
+
+    public Game() {
+    }
+
+    public void start() {
+        System.out.println("Game starting");
+    }
+
+    public static void main(String[] args) {
+        new Game().start();
+    }
+
+}
