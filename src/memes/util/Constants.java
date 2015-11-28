88ff34dc272d1bf54549ccba2de6da76d7ff2529
Joseph Groocock <frebib@gmail.com>
2015-11-28 21:30:32 +0000

    Added Port Number constant

--- src/memes/util/Constants.java
+++ src/memes/util/Constants.java
@@ -5,7 +5,10 @@ public class Constants {
     private Constants() {
     }
 
+    public static final int PORT_NUM = 13338;
+
     public static final int TILE_SIZE = 16;
     public static final int SPRITESHEET_HUMAN_SIZE = 32;
     public static final String SPRITESHEET_PATH = "res/sprites";
+
 }
