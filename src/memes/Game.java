4062fbfb13dafe04789b4c84368acc7c04d8d96c
Dom Williams <dwi3774@googlemail.com>
2015-11-28 17:32:16 +0000

    Made Game an actual Game

--- src/memes/Game.java
+++ src/memes/Game.java
@@ -1,16 +1,42 @@
 package memes;
 
-public class Game {
+import org.newdawn.slick.*;
+
+public class Game extends BasicGame {
 
     public Game() {
+        super("Pokememe");
     }
 
     public void start() {
         System.out.println("Game starting");
+
+        try {
+            AppGameContainer container = new AppGameContainer(this, 1280, 720, false);
+            container.start();
+        } catch (SlickException e) {
+            System.err.println("Failed to start game: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
     }
 
     public static void main(String[] args) {
         new Game().start();
     }
-
 }
