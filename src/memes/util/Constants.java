6d8a7ed1b1f504e6dd14b88d4a15787c68c015e3
Joseph Groocock <frebib@gmail.com>
2015-11-29 02:56:57 +0000

    Add handshake constants

--- src/memes/util/Constants.java
+++ src/memes/util/Constants.java
@@ -8,6 +8,8 @@ public class Constants {
     public static final Point WINDOW_SIZE = new Point(1280, 720);
 
     public static final int PORT_NUM = 13338;
+    public static final String HANDSHAKE_CLIENT = "ararepepe";
+    public static final String HANDSHAKE_SERVER = "dankburrito";
 
     public static final int TILE_LOAD_SIZE = 16;
     public static final int TILE_SIZE = 32;
